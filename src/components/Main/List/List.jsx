5bdda64b0285d9f0693e70b97040a6ef3973c2ab
Daniel Khabibullin <i6unny@gmail.com>
2023-05-26 18:54:31 +0500

    Add useBestPosts fix

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,18 +1,16 @@
 import {useContext} from 'react';
 import {postsContext} from '../../../context/postsContext';
 import style from './List.module.css';
-// import Post from './Post';
+import Post from './Post';
 
 export const List = () => {
-	const postsData = useContext(postsContext);
-	console.log(postsData);
+	const postsData = useContext(postsContext).posts;
 	return (
 		<ul className={style.list}>
-			{/* {
+			{
 				postsData.map((postData, index) => (<Post key={index}
 					postData={postData} />))
-			} */}
+			}
 		</ul>
 	);
 };
-
--- src/hooks/useBestPosts.js
+++ src/hooks/useBestPosts.js
@@ -5,30 +5,36 @@ import {tokenContext} from '../context/tokenContext';
 export const useBestPosts = () => {
 	const {token} = useContext(tokenContext);
 	const [posts, setPosts] = useState([]);
-
-	useEffect(() => {
-		if (!token) return;
-		fetch(`${URL_API}/best`, {
-			headers: {
-				Authorization: `bearer ${token}`,
-			},
-		})
-			.then(response => response.json())
-			.then((data) => {
-				const postData = data.data.children.map(({data: {
-					title, thumbnail, author, ups, created}}) => ({
+	const fetchPosts = async () => {
+		try {
+			const response = await fetch(`${URL_API}/best`, {
+				headers: {
+					Authorization: `bearer ${token}`,
+				},
+			});
+			const data = await response.json();
+			const postData = data.data.children.map(({
+				data: {title, thumbnail, author, ups, created}}) =>
+				({
 					title,
 					thumbnail: /^https:\/\//.test(thumbnail) ?
 						thumbnail.replace(/\?.*$/, '') : '',
 					author,
 					ups,
-					date: created,
+					date: created
 				}));
-				console.log(data.data.children);
-				setPosts(postData);
-			})
-			.catch(err => console.error(err));
-	}, []);
+
+			setPosts(postData);
+		} catch (err) {
+			console.warn(err);
+		}
+	};
+
+	useEffect(() => {
+		if (!token) return;
+
+		fetchPosts();
+	}, [token]);
 
 	return [posts];
 };
