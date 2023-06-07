696b25c64ea57fdf26a46346fe18fdca4516ee38
Daniel Khabibullin <i6unny@gmail.com>
2023-06-07 14:00:05 +0500

    Add redirect_uri fix

--- src/api/const.js
+++ src/api/const.js
@@ -4,6 +4,6 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'ZFUiUxIlXgK-p-1onDIi4Q';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
-// https://blogget-three.vercel.app/auth
+export const REDIRECT_URI = 'https://blogget-three.vercel.app/auth';
+// http://localhost:3000/auth
 export const SCOPE = 'identity submit read';
